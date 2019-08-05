827660da59bdf12e8cdb9fe5db5535daf4159aeb
PabloGarcia <pablo.garcia366@gmail.com>
2019-08-05 20:41:42 -0300

    Redux async

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,65 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
+import ErrorBoundry from '../components/ErrorBoundry';
 // import { robots } from '../robots';
 import './App.css';
 import Scroll from '../components/Scroll';
 
+import { setSearchFields, requestRobots } from '../actions';
 
+const mapStateToProps = state => {
+    // return { searchFields: state.searchRobots.searchFields };
+    return {
+        searchFields: state.SearchRobots.searchFields,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    };
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchFields(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    };
+}
 
 class App extends Component {
 
-    constructor() {
+    /* constructor() {
         super();
         this.state = {
             robots: [],
-            searchFields: ''
         }
-    }
+    } */
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+        this.props.onRequestRobots();
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchFields: event.target.value })
-    }
-
+    /*     onSearchChange = (event) => {
+            this.setState({ searchFields: event.target.value })
+        }
+     */
     render() {
-        const { robots, searchFields } = this.state;
+        const { searchFields, onSearchChange, robots, isPending } = this.props; // Redux*
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchFields.toLowerCase());
         });
-        return !robots.length ?
+        return isPending ?
             <h1> Loading .. </h1>
             :
             (
                 <div className="tc">
                     <h1 className="f1" > RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
@@ -49,4 +68,4 @@ class App extends Component {
 }
 
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { SearchRobots, requestRobots } from './reducers';
+
+// middleware
+import { createLogger } from 'redux-logger';
+
+// middleware para async 
+import thunkMiddleware from 'redux-thunk';
+
+// Containers
 import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
+
+// Para clases Css
+import './index.css';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Se combinan los reducers para agregarlos al store
+const rootReducers = combineReducers({ SearchRobots, requestRobots });
+
+// Crear store con los reducers y usar los middlewars
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+
+    <Provider store={store}>
+        <App />
+    </Provider>
+
+    , document.getElementById('root'));
+
 
 
